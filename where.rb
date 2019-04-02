c91b37b626db6dcc79410d7a3d5e1553c00e7cc7
Thomas Lee <leethomasjae@gmail.com>
2019-04-02 18:36:16 -0500

    begin implementing #where

--- where.rb
+++ where.rb
@@ -1,4 +1,13 @@
 module Where
-  def where(condition)
+  def where(conditions)
+    query_result = []
+
+    each do |record|
+      conditions.each do |column_name, value|
+        query_result << record if record[column_name] == value
+      end
+    end
+
+    query_result
   end
 end
